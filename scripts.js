e19061ee96ec911147fa1b6355d112572ba0308a
niceninja99 <rimonfahad11@gmail.com>
2025-02-19 21:33:07 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function creeperExplosion() {
+    const creeperImage = document.getElementById('creeper-image');
+    creeperImage.src = 'explode.gif'; // Change to the path of your explosion GIF
+    alert("Boom! The creeper exploded!");
+}
